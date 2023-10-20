b570738e16b345232de64df19a3ea53e8b013470
Cory Chilton <coryhookchilton@gmail.com>
2023-10-20 15:08:11 -0700

    Show added projects and todos in dom

--- src/domController.js
+++ src/domController.js
@@ -1,27 +1,42 @@
 import { logicController } from "./logicController";
 
 export const domController = (function () {
-  const todosDiv = document.getElementById('todos');
-  const projectsDiv = document.getElementById('projects');
+  const todosUl = document.getElementById('todos');
+  const projectsUl = document.getElementById('projects');
   const addTodoBtn = document.getElementById('add-todo');
   const addProjectBtn = document.getElementById('add-project');
   addTodoBtn.addEventListener('click', addTodoBtnClick);
   addProjectBtn.addEventListener('click', addProjectBtnClick);
 
   function addTodoBtnClick(){
-    logicController.addTodo('title', 'desc', 'due', 'priority', 'notes');
+    logicController.addTodo('todo title', 'desc', 'due', 'priority', 'notes');
     render();
   }
 
   function addProjectBtnClick() {
-    logicController.addProject('title');
+    logicController.addProject('project-title');
     render();
   }
 
   function render(){
+    projectsUl.textContent = "";
+    todosUl.textContent = "";
+
     const todos = logicController.getTodos();
     const projects = logicController.getProjects();
-    console.log({todos, projects});
+    todos.forEach((todo, idx) => {
+      const todoLi = document.createElement('li');
+      todoLi.textContent = todo.title;
+      todoLi.setAttribute('data-index', idx);
+      todosUl.appendChild(todoLi);
+    });
+
+    projects.forEach((project, idx) => {
+      const projectLi = document.createElement('li');
+      projectLi.textContent = project.title;
+      projectLi.setAttribute('data-index', idx);
+      projectsUl.appendChild(projectLi);
+    });
   }
 
   render();
