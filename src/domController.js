f4305e15f1eaf6e5ed09b59cdcef17bbe2734d73
Cory Chilton <coryhookchilton@gmail.com>
2023-10-20 14:57:23 -0700

    Add add project functionality

--- src/domController.js
+++ src/domController.js
@@ -2,17 +2,26 @@ import { logicController } from "./logicController";
 
 export const domController = (function () {
   const todosDiv = document.getElementById('todos');
-  const createTodoBtn = document.getElementById('add-todo');
-  createTodoBtn.addEventListener('click', addTodoBtnClick);
+  const projectsDiv = document.getElementById('projects');
+  const addTodoBtn = document.getElementById('add-todo');
+  const addProjectBtn = document.getElementById('add-project');
+  addTodoBtn.addEventListener('click', addTodoBtnClick);
+  addProjectBtn.addEventListener('click', addProjectBtnClick);
 
   function addTodoBtnClick(){
     logicController.addTodo('title', 'desc', 'due', 'priority', 'notes');
     render();
   }
 
+  function addProjectBtnClick() {
+    logicController.addProject('title');
+    render();
+  }
+
   function render(){
     const todos = logicController.getTodos();
-    console.log(todos);
+    const projects = logicController.getProjects();
+    console.log({todos, projects});
   }
 
   render();
--- src/logicController.js
+++ src/logicController.js
@@ -1,12 +1,21 @@
-import createTodo from "./todo";
 import createProject from "./project";
 
 export const logicController = (function() {
   let curProject = createProject('General');
+  const projects = [curProject];
 
   function addTodo(title, description, dueDate, priority, notes){
     curProject.addTodo(title, description, dueDate, priority, notes)
   }
 
-  return { addTodo, getTodos: curProject.getTodos }
+  function addProject(title) {
+    const project = createProject(title);
+    projects.push(project);
+  }
+
+  function getProjects() {
+    return projects;
+  }
+
+  return { addTodo, getTodos: curProject.getTodos, addProject, getProjects }
 })();
